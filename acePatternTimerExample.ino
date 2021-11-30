8a17a444d0a14988f63337ac4a4379cd866e3441
Goran Grubić <goran.grubic@gmail.com>
2021-11-30 06:19:31 +0100

    example

---
+++ acePatternTimerExample.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		acePatternTimerExample.ino
+ Created:	11/30/2021 6:14:22 AM
+ Author:	Korisnik
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
