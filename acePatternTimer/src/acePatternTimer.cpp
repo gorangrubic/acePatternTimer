7c79f4e6300fde5ad9eb0ea936703c0b0d2cdca7
Goran Grubić <goran.grubic@gmail.com>
2021-11-30 06:04:20 +0100

    Add project files.

---
+++ acePatternTimer/src/acePatternTimer.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		acePatternTimer.cpp
+ Created:	11/30/2021 6:03:16 AM
+ Author:	Korisnik
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "acePatternTimer.h"
+
+
---
+++ acePatternTimer/src/acePatternTimer.h
@@ -0,0 +1,19 @@
+/*
+ Name:		acePatternTimer.h
+ Created:	11/30/2021 6:03:16 AM
+ Author:	Korisnik
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _acePatternTimer_h
+#define _acePatternTimer_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
